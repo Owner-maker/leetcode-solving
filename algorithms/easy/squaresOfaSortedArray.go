d37077782d0f2bd8b317ad99d94a5a43bf8f977c
Artem Lisitsyn <lisitsyn.artem.d@gmail.com>
2023-04-22 02:41:48 +0400

    add empty solution to squaresOfaSortedArray task

---
+++ algorithms/easy/squaresOfaSortedArray.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func sortedSquares(nums []int) []int {
+	res := make([]int, len(nums), len(nums))
+
+	left := 0
+	right :=
+}
+
+func main() {
+	f := sortedSquares([]int{-4, -1, 0, 3, 10})
+	fmt.Print(f)
+}
